0b46a0c4f4f6afa139810994f1a53d4943561a61
Charles <charlesangeloplai@gmail.com>
2020-11-12 22:16:51 -0500

    commit tree f5e666af30c98027f9cad11d723a9a34e287bc85

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        result = ""
 
+        array.each_with_index do |element, index|
+            if array[index] == array.first
+                result << element.to_s
+            elsif array[index] == array.last
+                result << ", and " << element.to_s
+            else
+                result << ", " << element.to_s
+            end
+        end
+
+        return result
+    end
 end
